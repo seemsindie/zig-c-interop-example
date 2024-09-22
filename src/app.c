af152259480a9c1dcb4bef1eab72c8f2c61918b9
Ivan Stamenkovic <ivan@seemsindie.com>
2024-09-23 00:09:34 +0200

    Update some files

--- src/app.c
+++ src/app.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
 #include "lib.h"
+#include <stdio.h>
 
 extern int zig_function(int);
 
 int main() {
-    printf("C app: calling C library function: %d\n", c_function(5));
-    printf("C app: calling Zig library function: %d\n", zig_function(5));
-    return 0;
+  printf("C app: calling C library function: %d\n", c_function(5));
+  printf("C app: calling Zig library function: %d\n", zig_function(5));
+  return 0;
 }
--- src/app.zig
+++ src/app.zig
@@ -8,5 +8,3 @@ pub fn main() !void {
     std.debug.print("Zig app: calling C library function: {d}\n", .{c.c_function(5)});
     std.debug.print("Zig app: calling Zig library function: {d}\n", .{zig_function(5)});
 }
-
-extern fn c_function(x: c_int) c_int;
